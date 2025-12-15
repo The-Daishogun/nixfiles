4ebc8f912b2a2c36f1178b3a8785fec9d6cf68a5
Mohammadreza Varasteh <varastehmr@gmail.com>
2025-12-15 20:33:10 +0330

    fix: nvidia corruption issue

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -8,4 +8,9 @@ in {
   hardware.graphics.enable = true;
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia.open = lib.mkOverride 990 (nvidiaPackage ? open && nvidiaPackage ? firmware);
+
+  # to help with suspend corruption issue
+  powerManagement.enable = true;
+  hardware.nvidia.powerManagement.enable = true;
+  boot.kernelParams = [ "nvidia.NVreg_TemporaryFilePath=/var/tmp" ];
 }
