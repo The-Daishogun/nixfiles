9a04d9e61ca831fb4e6bac7612310013f4c19570
Mohammadreza Varasteh <varastehmr@gmail.com>
2025-12-15 15:09:21 +0330

    feat: even more gnome customizations

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -1,15 +1,26 @@
-{ pkgs, ...}: {
+{ lib, pkgs, ...}: {
   dconf.enable = true;
-  dconf.settings = {
-    "org/gnome/desktop/interface/color-scheme" = "prefer-dark";
+  dconf.settings = with lib.gvariant; {
     "org/gnome/desktop/interface" = {
       accent-color = "green";
+      color-scheme = "prefer-dark";
+      show-battery-percentage = true;
+      enable-hot-corners = false;
+      clock-show-weekday = true;
     };
     "org/gnome/desktop/input-sources" = {
+      sources = [
+        (mkTuple [ "xkb" "us" ]) 
+        (mkTuple [ "xkb" "ir" ])
+      ];
       xkb-options = [ "ctrl:nocaps" ];
     };
     "org/gnome/shell" = {
-        # disable-user-extensions = true; # Optionally disable user extensions entirely
+        favorite-apps = [
+          "google-chrome.desktop"
+          "code.desktop"
+          "org.gnome.Nautilus.desktop"
+        ];
         enabled-extensions = with pkgs; [
           gnomeExtensions.appindicator.extensionUuid
           gnomeExtensions.autohide-battery.extensionUuid
@@ -19,12 +30,33 @@
           gnomeExtensions.proxy-switcher.extensionUuid
         ];
       };
+    "org/gnome/mutter" = {
+      workspaces-only-on-primary = false;
+    };
+    "org/gnome/settings-daemon/plugins/media-keys"= {
+      custom-keybindings = {
+        custom0 = {
+          binding = "<Super>Return";
+          command = "alacritty";
+          name = "terminal";
+        };
+      };
+    };
+    "org/gnome/desktop/wm/keybindings" = {
+      close = [
+        "<Super>w"
+      ];
+    };
+    
+    "org/gnome/desktop/wm/preferences" = {
+      button-layout = "appmenu:minimize,maximize,close";
+    };
   };
 
   qt = {
     enable = true;
     platformTheme = "qtct";
-    style = "kvantum";
+    style.name = "kvantum";
   };
 
   xdg.configFile = {
