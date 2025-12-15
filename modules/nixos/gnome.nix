20f0a8148c829da76a7146e7e99928d3e2ea4b2b
Mohammadreza Varasteh <varastehmr@gmail.com>
2025-12-15 12:24:46 +0330

    feat: remove gnome bloat

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -1,3 +1,17 @@
-{...}:{
+{ pkgs, ...}:{
   services.desktopManager.gnome.enable = true;
+  services.gnome.core-developer-tools.enable = false;
+  services.gnome.games.enable = false;
+  environment.gnome.excludePackages = with pkgs; [ 
+    gnome-tour
+    gnome-user-docs
+    gnome-music
+    gnome-photos
+    gnome-clocks
+    gnome-console
+    gnome-terminal
+    gnome-contacts
+    gnome-maps
+    gnome-calendar
+    ];
 }
