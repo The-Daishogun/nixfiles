fbc48f661354d24710d073b54af377fa90ca122c
Mohammadreza Varasteh <varastehmr@gmail.com>
2025-12-19 02:19:11 +0330

    fix: disable the builtin monitor in hyprland

--- modules/home-manager/wm/hyprland/default.nix
+++ modules/home-manager/wm/hyprland/default.nix
@@ -7,6 +7,7 @@
         # Example: Primary monitor
         "HDMI-A-1, 2560x1440@120, 0x0, 1"
         "DP-1, 1920x1080@60, auto, 1, transform, 3"
+        "eDP-1, disabled"
         # If you have multiple monitors, add them here:
         # "HDMI-A-1, 1920x1080@60, 0x0, 1"
       ];
