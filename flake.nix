d43621deb45ca4088520ee98b9bbc9c99efa1512
Mohammadreza Varasteh <varastehmr@gmail.com>
2025-12-21 00:27:53 +0330

    feat: add noctalia

--- flake.nix
+++ flake.nix
@@ -12,44 +12,52 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixvim,
-    home-manager,
-    ...
-  } @ inputs: {
-    nixosConfigurations = {
-      daipc = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./hosts/daipc/configuration.nix
-          ./modules/nixos/pkgs/default.nix
-          ./modules/nixos/pkgs/hyprland.nix
-          ./modules/nixos/nvidia.nix
-          ./modules/nixos/bluetooth.nix
-          ./modules/nixos/sound.nix
-          ./modules/nixos/input.nix
-          ./modules/nixos/xdg.nix
-          ./modules/nixos/display.nix
-          ./modules/nixos/gnome.nix
-          ./modules/nixos/hyprland.nix
-          ./modules/nixos/networking.nix
-          ./modules/nixos/zsh.nix
-          ./modules/nixos/containers.nix
-        ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixvim,
+      noctalia,
+      home-manager,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations = {
+        daipc = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/daipc/configuration.nix
+            ./modules/nixos/pkgs/default.nix
+            ./modules/nixos/pkgs/hyprland.nix
+            ./modules/nixos/nvidia.nix
+            ./modules/nixos/bluetooth.nix
+            ./modules/nixos/sound.nix
+            ./modules/nixos/input.nix
+            ./modules/nixos/xdg.nix
+            ./modules/nixos/display.nix
+            ./modules/nixos/gnome.nix
+            ./modules/nixos/hyprland.nix
+            ./modules/nixos/networking.nix
+            ./modules/nixos/zsh.nix
+            ./modules/nixos/containers.nix
+          ];
+        };
+      };
+      homeConfigurations = {
+        daishogun = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [
+            nixvim.homeModules.nixvim
+            noctalia.homeModules.default
+            ./hosts/daipc/home.nix
+          ];
+        };
       };
     };
-    homeConfigurations = {
-      daishogun = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        modules = [
-          nixvim.homeModules.nixvim
-          ./hosts/daipc/home.nix
-        ];
-      };
-    };
-  };
 }
