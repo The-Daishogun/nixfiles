70dc18a8cf87826801898db4a31ee66b600ee659
Mohammadreza Varasteh <varastehmr@gmail.com>
2025-12-22 19:26:04 +0330

    temp: change to 60hz

--- modules/home-manager/wm/hyprland/default.nix
+++ modules/home-manager/wm/hyprland/default.nix
@@ -7,7 +7,7 @@
     enable = true;
     settings = {
       monitor = [
-        "HDMI-A-1, 2560x1440@120, 0x0, 1"
+        "HDMI-A-1, 2560x1440@60, 0x0, 1"
         "DP-1, 1920x1080@60, auto, 1, transform, 3"
         "eDP-1, disabled"
       ];
