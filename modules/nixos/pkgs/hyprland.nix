38de705802542040e34bfebf88b87400dbf9656d
Mohammadreza Varasteh <varastehmr@gmail.com>
2026-01-21 23:49:07 +0330

    feat: disable hyprland

--- modules/nixos/pkgs/hyprland.nix
+++ modules/nixos/pkgs/hyprland.nix
@@ -1,9 +1,9 @@
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    hyprlauncher # Application launcher/switcher
-    hyprpaper
-    hyprlock
-  ];
-  programs.nm-applet.enable = true;
-  programs.nm-applet.indicator = true;
+  # environment.systemPackages = with pkgs; [
+  #   hyprlauncher # Application launcher/switcher
+  #   hyprpaper
+  #   hyprlock
+  # ];
+  # programs.nm-applet.enable = true;
+  # programs.nm-applet.indicator = true;
 }
