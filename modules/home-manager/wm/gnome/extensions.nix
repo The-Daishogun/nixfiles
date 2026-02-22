258aa0936f7b4682dc783d3aba4c9799823e6f1e
Mohammadreza Varasteh <varastehmr@gmail.com>
2026-02-23 00:39:43 +0330

    feat: remove tailscaleqs

--- modules/home-manager/wm/gnome/extensions.nix
+++ modules/home-manager/wm/gnome/extensions.nix
@@ -8,7 +8,6 @@
     gnomeExtensions.proxy-switcher
     gnomeExtensions.user-themes
     gnomeExtensions.paperwm
-    gnomeExtensions.tailscale-qs
   ];
 
   dconf.settings = {
@@ -22,7 +21,6 @@
         gnomeExtensions.proxy-switcher.extensionUuid
         gnomeExtensions.user-themes.extensionUuid
         gnomeExtensions.paperwm.extensionUuid
-        gnomeExtensions.tailscale-qs.extensionUuid
       ];
     };
   };
